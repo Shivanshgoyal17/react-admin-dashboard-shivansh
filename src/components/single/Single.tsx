5d02898578883a53ab4a8571f0772ebf06a52cd5
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2026-02-06 19:56:52 +0530

    Fix unused import error

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -1,4 +1,4 @@
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 import "./single.scss"
 
 type Props = {
